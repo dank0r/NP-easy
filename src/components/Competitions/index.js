fc1c79ac98889b8c565876760ee08e5d8a95eae6
dank0r <dan296dan@gmail.com>
2020-05-03 15:50:23 +0300

    add header

--- src/components/Competitions/index.js
+++ src/components/Competitions/index.js
@@ -1,13 +1,9 @@
 import React from 'react';
 import styles from './index.module.css';
-import Header from '../Header';
-import Competitions from '../Competitions';
 
 function MainPage(props) {
   return (
     <div className={styles.container}>
-      <Header className={styles} />
-      <Competitions />
     </div>
   );
 }
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import styles from './index.module.css';
-import Header from '../Header';
-import Competitions from '../Competitions';
 
 function MainPage(props) {
   return (
     <div className={styles.container}>
-      <Header className={styles} />
-      <Competitions />
+      <div className={styles.logo}>
+        NP-easy
+      </div>
     </div>
   );
 }
