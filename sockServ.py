237ba10824be951d72e8b3c1b048e4b5c74fd66d
h4ckit <h4ckit@mail.ru>
2020-05-07 15:42:35 +0300

    Web checker 9090

--- sockServ.py
+++ sockServ.py
@@ -7,14 +7,28 @@ sock = socket.socket()
 sock.bind(('', 9090))
 sock.listen(1)
 
+str = """<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<script type="text/javascript">window.close();</script>
+</head>
+<body>
+</body>
+</html>"""
+
 while True:
     try:
         conn, addr = sock.accept()
         print('connected:', addr)
-        data = conn.recv(1024)
+        data = conn.recv(2048)
         if not data:
             break
-        conn.send(data.upper())
+
+        if b'GET' in data:
+            conn.send(bytes('HTTP/1.1 200 OK\r\n\r\n', 'UTF-8'))
+        else:
+            pass
         conn.close()
     except:
         pass
