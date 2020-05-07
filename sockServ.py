7a0899f4f40fff665299c3f5ba4bbdf7e910e137
h4ckit <h4ckit@mail.ru>
2020-05-07 12:59:34 +0300

    Loop

--- sockServ.py
+++ sockServ.py
@@ -6,14 +6,15 @@ import socket
 sock = socket.socket()
 sock.bind(('', 9090))
 sock.listen(1)
-conn, addr = sock.accept()
-
-print('connected:', addr)
 
 while True:
-    data = conn.recv(1024)
-    if not data:
-        break
-    conn.send(data.upper())
-
-conn.close()
+    try:
+        conn, addr = sock.accept()
+        print('connected:', addr)
+        data = conn.recv(1024)
+        if not data:
+            break
+        conn.send(data.upper())
+        conn.close()
+    except:
+        pass
