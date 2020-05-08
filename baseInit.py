6635b0bb3792d4d8446fa5ebaef51a4a5819a832
root <h4ckit@mail.ru>
2020-05-08 13:11:38 +0300

    base dependenses, some sctipts for front

--- baseInit.py
+++ baseInit.py
@@ -8,6 +8,16 @@ def exc(s):
     rows = cur.fetchall()
     for i in rows:
         print(i)
+    print(':::::executed:::::')
+    print(s)
 
-exc("DROP TABLE IF EXISTS events;")
-exc("DROP TABLE IF EXISTS events;")
+exc("""drop table if exists events;""")
+exc("""create table events (id int(11) auto_increment, title varchar(40), previewURL varchar(250), briefDescription varchar(300), description varchar(500), teams int(11), deadline varchar(30), constraint event_pk primary key (id))CHARSET=utf8;""")
+
+
+exc("""insert into events values(1, "Travelling Salesman Person", "some_url", "Определить кратчайший путь, проходящий через все вершины графа", "Long description", 3, "07.07.2020 00:00");""")
+exc("""insert into events values(2, "Travelling Salesman Person", "some_url", "Определить кратчайший путь, проходящий через все вершины графа", "Long description", 3, "07.07.2020 00:00");""")
+
+
+exc("""select * from events;""")
+exc("""commit;""")
--- sockServ.py
+++ sockServ.py
@@ -1,25 +1,46 @@
-import json
+import pymysql  
 
-
-from flask import Flask
+from flask import Flask, jsonify, send_file, request
 from flask_cors import CORS
 app = Flask(__name__)
+app.config['JSON_AS_ASCII'] = False
 CORS(app)
 
-import pymysql  
-con = pymysql.connect('localhost', 'npadmin',  'nppass', 'npeasy')
-
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
     try:
-        cur = con.cursor()
-        cur.execute("SELECT * FROM events")
-
-        rows = cur.fetchall()
-        st = json.dumps(rows)
-        return st
+        if request.method == 'GET':
+            con = pymysql.connect('localhost', 'npadmin',  'nppass', 'npeasy')
+            cur = con.cursor()
+            cur.execute("SELECT * FROM events")
+            pt = []
+            rows = cur.fetchall()
+            for i in rows:
+                snd = {}
+                snd['id'] = i[0]
+                snd['title'] = i[1]
+                snd['previewURL'] = i[2]
+                snd['briefDescription'] = i[3]
+                snd['description'] = i[4]
+                snd['teams'] = i[5]
+                snd['deadline'] = i[6]
+                pt.append(snd)
+            return jsonify(pt)
+        elif request.method == 'POST':
+            content = request.json
+            print(content)
+            dd = {"responseStatus": "success", "error": "null", "solutions": [{"competitionId": 1, "solution": "some_code", "compiler": "c++", "submissionDateTime": "07.05.2020 23:21", "status": "testing", "result": "", "time": ""}]} 
+            return jsonify(dd)
     except Exception as e:
-        return str(e)
+        return 'ERROR: ' + str(e)
+
+    
+@app.route('/images/<name>', methods=['POST', 'GET'])
+def images(name):
+    try:
+        return send_file('../images/' + name)
+    except Exception as e:
+        return 'ERROR: ' + str(e)
 
 
 if __name__ == "__main__":
