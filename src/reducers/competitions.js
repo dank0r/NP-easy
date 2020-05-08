8be1b6e930c15c67cfdebd8821f190c40b003fd0
dank0r <dan296dan@gmail.com>
2020-05-08 11:46:11 +0300

    fix bug

--- src/reducers/competitions.js
+++ src/reducers/competitions.js
@@ -1,10 +1,19 @@
-const competitions = (state={list: [], isLoading: false, error: null}, action) => {
+const initialList = [
+
+];
+
+const competitions = (state={list: initialList, isLoading: false, error: null}, action) => {
   console.log(action);
   switch (action.type) {
     case 'FETCH_COMPETITIONS_REQUEST':
       return {...state, isLoading: true};
     case 'FETCH_COMPETITIONS_SUCCESS':
-      return {...state, isLoading: false, list: [...state.list, action.payload.data]};
+      let newList = state.list;
+      if(action.payload.data !== null) {
+        newList = newList.concat(action.payload.data);
+      }
+      console.log(newList);
+      return {...state, isLoading: false, list: newList};
     case 'FETCH_COMPETITIONS_FAILURE':
       return {...state, isLoading: false, error: 'err'};
     default:
