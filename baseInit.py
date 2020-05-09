6978816e1bae1f681ddfed40d663bf3f7146c56a
root <h4ckit@mail.ru>
2020-05-09 16:11:46 +0300

    add Logs, token and authorization

--- baseInit.py
+++ baseInit.py
@@ -13,22 +13,27 @@ def exc(s):
 
     
 
-##################################################### events
+##################################################### events(title, previewURL, briefDescription, description, teams, deadline)
 exc("""drop table if exists events;""")
 exc("""create table events (id int(11) auto_increment, title varchar(40), previewURL varchar(250), briefDescription varchar(300), description varchar(500), teams int(11), deadline varchar(30), constraint event_pk primary key (id)) CHARSET=utf8;""")
 
-exc("""insert into events values(1, "Travelling Salesman Person", "some_url", "Определить кратчайший путь, проходящий через все вершины графа", "Long description", 3, "07.07.2020 00:00");""")
-exc("""insert into events values(2, "Travelling Salesman Person", "some_url", "Определить кратчайший путь, проходящий через все вершины графа", "Long description", 3, "07.07.2020 00:00");""")
+exc("""insert into events(title, previewURL, briefDescription, description, teams, deadline) values("Travelling Salesman Person", "some_url", "Определить кратчайший путь, проходящий через все вершины графа", "Long description", 3, "07.07.2020 00:00");""")
+exc("""insert into events(title, previewURL, briefDescription, description, teams, deadline) values("Travelling Salesman Person", "some_url", "Определить кратчайший путь, проходящий через все вершины графа", "Long description", 3, "07.07.2020 00:00");""")
 
 
 
-##################################################### users
-#{id: 1, username: "user", email: "something", passHash: "something", avatarURL: "something"}
+##################################################### users (username, email, passHash, avatarURL)
 exc("""drop table if exists users;""")
-exc("""create table events (id int(11) auto_increment, username varchar(40), email varchar(40), passHash varchar(300), avatarURL varchar(500), constraint user_pk primary key (id)) CHARSET=utf8;""")
+exc("""create table users (id int(11) auto_increment, username varchar(40), email varchar(40), passHash varchar(300), avatarURL varchar(500), constraint user_pk primary key (id)) CHARSET=utf8;""")
 
-exc("""insert into users values(1, "MigmasADMIN", "h4ckit@mail.ru", "somehash123123123", "nourl");""")
-exc("""insert into users values(2, "user", "something", "something", "something");""")
+exc("""insert into users(username, email, passHash, avatarURL) values("MigmasADMIN", "h4ckit@mail.ru", "somehash123123123", "nourl");""")
+exc("""insert into users(username, email, passHash, avatarURL) values("user", "something", "something", "something");""")
+
+
+
+##################################################### tokens (username, token)
+exc("""drop table if exists tokens;""")
+exc("""create table tokens (id int(11) auto_increment, username varchar(40), token varchar(32), constraint token_pk primary key (id)) CHARSET=utf8;""")
 
 
 
