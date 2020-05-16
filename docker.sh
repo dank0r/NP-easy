77185b3c749aeeafdf2c4f43c807ad49f5bac4b6
LightVillet <maxim12000@ya.ru>
2020-05-16 03:19:55 +0300

    Stable socket server

--- docker.sh
+++ docker.sh
@@ -4,11 +4,13 @@
 #echo "COPY ./input ./root/input" >> Dockerfile
 #echo "CMD cd /root/ && python3 $1" >> Dockerfile
 #docker --log-level fatal build -t t .
+echo "" > ./$1/output
+echo "" > ./$1/return_code
 docker --log-level error run -dt --name solution_$1 main 
 docker --log-level error cp ./$1/$2 $(docker ps -a | grep "solution_$1" | cut -d " " -f 1):/root/$2
 #rm ./$1/$2
-docker --log-level error exec -t solution_$1 bash -c "cd /root/ && python3 $2"
-echo "" > ./$1/output
+docker --log-level error exec -t solution_$1 bash -c "cd /root/ && python3 $2 && echo \$? > return_code"
 docker --log-level error cp $(docker ps -a | grep "solution_$1" | cut -d " " -f 1):/root/output ./$1/output
+docker --log-level error cp $(docker ps -a | grep "solution_$1" | cut -d " " -f 1):/root/return_code ./$1/return_code
 docker --log-level error stop solution_$1
 docker --log-level error system prune -f
--- server.c
+++ server.c
@@ -5,25 +5,40 @@
 #include <arpa/inet.h>
 #include <string.h>
 #include <unistd.h>
+#include <setjmp.h>
+#include <signal.h>
 
 const unsigned int port = 1100; //порт для связи
 const int VERY_BIG = 1024 * 1024;
+const int MAX_LEN = 1024;
+
+int socConnectFD;
 
 char* concat(const char *s1, const char *s2)
 {
-    char *result = malloc(strlen(s1) + strlen(s2) + 1);
-    strcpy(result, s1);
-    strcat(result, s2);
-    return result;
+	char *result = malloc(strlen(s1) + strlen(s2) + 1);
+	strcpy(result, s1);
+    	strcat(result, s2);
+	return result;
 }
+
+void signalEndHandler(int signum)
+{
+	shutdown(socConnectFD, SHUT_RDWR);
+	close(socConnectFD);
+	printf("\nExiting...\n");
+	exit(signum);
+}
+
+
 
 int main()
 {
+	signal(SIGINT, signalEndHandler);
 	char buf[VERY_BIG]; //буфер для получения данных
-	char name[1024];
+	char name[MAX_LEN];
 	char actionId[2];
-	char outputBuf[VERY_BIG];
-	char solutionId[1024];
+	char solutionId[MAX_LEN];
 	struct sockaddr_in stSockAddr;
 	stSockAddr.sin_family = PF_INET;
 	stSockAddr.sin_port = htons (port); 
@@ -49,9 +64,9 @@ int main()
 
 	for (;;)
 	{
-		struct  sockaddr_in client_addr; 
+		struct sockaddr_in client_addr; 
 		int cl_size = sizeof(client_addr);
-		int socConnectFD = accept(mySocket, (struct sockaddr*) &client_addr, (socklen_t*)&cl_size);
+		socConnectFD = accept(mySocket, (struct sockaddr*) &client_addr, (socklen_t*)&cl_size);
 		if (socConnectFD < 0)
 		{
 			printf("Error accepting data!\n");
@@ -59,86 +74,90 @@ int main()
 			return -4;
 		}
                 //printf ("%d/", ntohs(client_addr.sin_addr.s_addr));
-		try:
+		read(socConnectFD, solutionId, sizeof(solutionId));
+		printf("\nsolutionId %s\n", solutionId);
+		write(socConnectFD, "OK", sizeof("OK"));
+		read(socConnectFD, actionId, sizeof(actionId));
+		printf("\nactionId %s\n", actionId);
+		switch (actionId[0])
 		{
-			read(socConnectFD, solutionId, sizeof(solutionId));
-			printf("\nsolutionId %s\n", solutionId);
-			write(socConnectFD, "OK", sizeof("OK"));
-			read(socConnectFD, actionId, sizeof(actionId));
-			printf("\nactionId %s\n", actionId);
-			switch (actionId[0])
+			case '0':
 			{
-				case '0':
-				{
-					write(socConnectFD, "OK", sizeof("OK"));
-					system(concat("mkdir ", solutionId));
-					read(socConnectFD, name, sizeof(name));
-					write(socConnectFD, "OK", sizeof("OK"));
-					read(socConnectFD, buf, sizeof(buf));
-					write(socConnectFD, "OK", sizeof("OK"));
-					char fpPath[1024];
-					sprintf(fpPath, "./%s/%s", solutionId, name);
-					FILE *fp = fopen(fpPath, "wb");
-					fprintf(fp, "%s", buf);
-					fclose(fp);
-					char runCommand[1024];
-					sprintf(runCommand, "./docker.sh %s %s &", solutionId, name);
-					//sprintf(runCommand, "./docker.sh %s %s", solutionId, name);
-					system(runCommand);
-					system("disown");
+				write(socConnectFD, "OK", sizeof("OK"));
+				int resultMkdir = system(concat("mkdir ", solutionId));
+				read(socConnectFD, name, sizeof(name));
+				write(socConnectFD, "OK", sizeof("OK"));
+				read(socConnectFD, buf, sizeof(buf));
+				write(socConnectFD, "OK", sizeof("OK"));
+				if (resultMkdir == -1)
 					break;
-				}
-				case '1':
+				char fpPath[MAX_LEN];
+				sprintf(fpPath, "./%s/%s%c", solutionId, name, '\0');
+				FILE *fp = fopen(fpPath, "wb");
+				fprintf(fp, "%s", buf);
+				fclose(fp);
+				char runCommand[MAX_LEN];
+				sprintf(runCommand, "./docker.sh %s %s &%c", solutionId, name, '\0');
+				//sprintf(runCommand, "./docker.sh %s %s", solutionId, name);
+				system(runCommand);
+				break;
+			}
+			case '1':
+			{
+				char returnCodePath[MAX_LEN];
+				sprintf(returnCodePath, "./%s/return_code%c", solutionId, '\0');
+				char returnCode[MAX_LEN];
+				FILE *returnCodeFile = fopen(returnCodePath, "r");
+				int returnCodeSize = fscanf(returnCodeFile, "%s", returnCode);
+				fclose(returnCodeFile);
+				if (returnCodeSize == -1)
 				{
-					char checkCommand[1024];
-					sprintf(checkCommand, "docker ps -a | grep \"solution_%s \" > ./%s/%s", solutionId, solutionId, solutionId);
-					system(checkCommand);
-					char psPath[1024];
-					sprintf(psPath, "./%s/%s", solutionId, solutionId);
-					FILE *psFile = fopen(psPath, "r");
-					fseek(psFile, 0, SEEK_END);
-					long int psSize = ftell(psFile);
-					fclose(psFile);
-					printf("\npsSize %ld\n", psSize);
-					if (psSize < 3)
+					write(socConnectFD, "WAIT", sizeof("WAIT"));
+				}
+				else
+				{
+					if (returnCode[0] == '0') 
+					{
 						write(socConnectFD, "END", sizeof("END"));
-					else
-						write(socConnectFD, "WAIT", sizeof("WAIT"));
-					break;
-				}
-				case '2':
-				{
-					char outputPath[1024];
-					sprintf(outputPath, "./%s/output", solutionId);
-					FILE *outputFile = fopen(outputPath, "w+");
-					fseek(outputFile, 0, SEEK_END);
-					long int outputSize = ftell(outputFile);
-					printf("%ld", outputSize);
-					rewind(outputFile);
-					if (outputSize == 0)
-						write(socConnectFD, "NO_OUTPUT", sizeof("NO_OUTPUT"));
+					}
 					else
 					{
-						fread(outputBuf, 1, outputSize, outputFile);
-						write(socConnectFD, outputBuf, sizeof(outputBuf));
+						write(socConnectFD, "CE", sizeof("CE"));
 					}
-					char rmCommand[1024];
-					sscanf(rmCommand, "rm -r %s", solutionId);
-					system(rmCommand);
-					fclose(outputFile);
-					break;
 				}
-				default:
-				{
-					write(socConnectFD, "ERROR", sizeof("ERROR"));
-				}
+				break;
 			}
-			//for (int i = 0; i < sizeof(name); i++) printf("%c\n", name[i]);
-			shutdown(socConnectFD, SHUT_RDWR);
-			close(socConnectFD);
+			case '2':
+			{
+				char outputPath[MAX_LEN];
+				sprintf(outputPath, "./%s/output%c", solutionId, '\0');
+				FILE *outputFile = fopen(outputPath, "r");
+				char *outputBuf;
+				ssize_t outputSize;
+				ssize_t tmp;
+				outputSize = getline(&outputBuf, &tmp, outputFile);
+				if (outputSize == 0)
+				{
+					write(socConnectFD, "NO_OUTPUT", sizeof("NO_OUTPUT"));
+				}
+				else
+				{
+					write(socConnectFD, outputBuf, outputSize);
+				}
+				free(outputBuf);
+				char rmCommand[MAX_LEN];
+				sprintf(rmCommand, "rm -r ./%s", solutionId);
+				system(rmCommand);
+				fclose(outputFile);
+				break;
+			}
+			default:
+			{
+				write(socConnectFD, "ERROR", sizeof("ERROR"));
+			}
 		}
+		shutdown(socConnectFD, SHUT_RDWR);
+		close(socConnectFD);
 	}
 	return 0;
 }
-
-
